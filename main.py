7ac047ea8f8795b55517d3b749fd8839429da4e4
archi-ka4alov <archi.ka4alov@gmail.com>
2024-08-28 20:29:06 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+home_work_count = 12
+hours_count = 1.5
+course_name = 'Python'
+one_task_time = hours_count / home_work_count
+print(course_name, ', всего задач: ', home_work_count, ', затрачено часов', hours_count, ', среднее время выполнения', one_task_time, 'часа.')
